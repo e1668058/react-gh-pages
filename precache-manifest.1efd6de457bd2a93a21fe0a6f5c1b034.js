7e8b61131efa0d869cea699336391745616b685f
Allen Jang <e1668058@cmaisonneuve.qc.ca>
2020-04-29 17:45:16 -0400

    Updates

---
+++ precache-manifest.1efd6de457bd2a93a21fe0a6f5c1b034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8c27f16f0c346307ff6e4215187575",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "6f258e16f9a6418d07f9",
+    "url": "/react-gh-pages/static/css/main.feb11f45.chunk.css"
+  },
+  {
+    "revision": "92134cfb05ac1ce905d2",
+    "url": "/react-gh-pages/static/js/2.1c2698b3.chunk.js"
+  },
+  {
+    "revision": "973b0157c75ad00a2bf6d9ba8bf3685b",
+    "url": "/react-gh-pages/static/js/2.1c2698b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f258e16f9a6418d07f9",
+    "url": "/react-gh-pages/static/js/main.64eb675b.chunk.js"
+  },
+  {
+    "revision": "f290e978aa106e6610c9",
+    "url": "/react-gh-pages/static/js/runtime-main.c2549955.js"
+  }
+]);
